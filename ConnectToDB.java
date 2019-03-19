40502c3ba3aa54b32e40d3f1e7e57634533aa3d0
Genc i <gencibalaj@gmail.com>
2019-03-19 14:17:12 +0100

    konektimi me databaze

---
+++ ConnectToDB.java
@@ -0,0 +1,31 @@
+package projektiKNK;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+
+
+	
+	
+	
+	
+
+
+
+	
+
+	
+
+	
+}
+
+
+	
+	
+	
