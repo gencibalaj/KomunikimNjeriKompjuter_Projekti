195c802501bffaf123c13458ad259feea09dd531
Genc i <gencibalaj@gmail.com>
2019-03-20 22:14:12 +0100

    ora

---
+++ ClockPane.java
@@ -0,0 +1,21 @@
+package projektiKNK;
+import javafx.animation.Animation;
+import javafx.animation.Timeline;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.animation.KeyFrame;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
