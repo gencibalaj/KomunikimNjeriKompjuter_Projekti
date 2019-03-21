1f4a14f1377ade66cffe038f4fccac87e47bc00b
Genc i <eltimusa4@gmail.com>
2019-03-21 14:45:12 +0100

    ora vazhdim

--- ClockPane.java
+++ ClockPane.java
@@ -48,4 +48,91 @@ public class ClockPane extends Pane {
 		paintClock();
 		animation = new Timeline(
 			new KeyFrame(Duration.millis(1000), e -> moveClock()));
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.play();
+	}
+
+	/** Return hour */
+	public int getHour() {
+		return hour;
+	}
+
+	/** Set a new hour */
+	public void setHour(int hour) {
+		this.hour = hour;
+		paintClock();
+	}
+
+	/** Return minute */
+	public int getMinute() {
+		return minute;
+	}
+
+	/** Set a new minute */
+	public void setMinute(int minute) {
+		this.minute = minute;
+		paintClock();
+	}
+
+	/** Return second */
+	public int getSecond() {
+		return second;
+	}
+
+	/** Set a new second */
+	public void setSecond(int second) {
+		this.second = second;
+		paintClock();
+	}
+
+	/** Return clock pane's width */
+	public double getW() {
+		return w;
+	}
+
+	/** Set clock pane's width */
+	public void getW(double w) {
+		this.w = w;
+		paintClock();
+	}
+
+	/** Return clock pane's height */
+	public double getH() {
+		return h;
+	}
+
+	/** Set clock pane's heigt */
+	public void setH(double h) {
+		this.h = h;
+		paintClock();
+	}
+
+	/* Set the current time for the clock */
+	public void setCurrentTime() {
+		// Construct a Calendar for the current date and time
+		Calendar calendar = new GregorianCalendar();
+
+		// Set current hour, minute and second
+		this.hour = calendar.get(Calendar.HOUR_OF_DAY);
+		this.minute = calendar.get(Calendar.MINUTE);
+		this.second = calendar.get(Calendar.SECOND);
+		paintClock(); // Repaint the clock
+	}
+	
+	/** Paint the clock */
+	protected void paintClock() {
+		
+		// Initialize clock parameters
+		double clockRadius = 100/2;
+		double centerX = w / 2.0;
+		double centerY = h / 2.0;
+
+		// Draw circle
+		Circle circle = new Circle(centerX, centerY, clockRadius);
+		circle.setFill(Color.TRANSPARENT);
+		circle.setStroke(Color.BLACK);
+		getChildren().clear();
+		for(int i = 0; i < 12; i++) {
+			Text t = new Text(Math.cos(i*Math.PI/6-Math.PI/3)*clockRadius+centerX,Math.sin(i*Math.PI/6-Math.PI/3)*clockRadius+centerY,(i+1)+"");
+			t.setStyle("-fx-font-weight: bold;-fx-font-size:10");
 	
