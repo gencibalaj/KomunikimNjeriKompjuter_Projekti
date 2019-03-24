69e4c2479105af88bd131821420475e862dab616
Genc i <ditjonthaqi@hotmail.com>
2019-03-24 17:45:12 +0100

    pacientat

---
+++ AddPacient.java
@@ -0,0 +1,24 @@
+package projektiKNK;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoPeriod;
+import java.time.chrono.Chronology;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+
+public class AddPacient extends GridPane {
