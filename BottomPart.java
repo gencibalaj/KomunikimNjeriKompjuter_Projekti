cc35aa7d706ddf4e3bd78bd7b897924064ae5589
Genc i <eltimusa4@gmail.com>
2019-03-27 20:45:12 +0100

    k

---
+++ BottomPart.java
@@ -0,0 +1,16 @@
+package projektiKNK;
+
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
