6a3c7378c8daf3eb52f9ce04f8dcbad517946463
Genc i <ditjonthaqi@gmail.com>
2019-04-01 14:45:12 +0200

    ffffff

---
+++ AddVisit.java
@@ -0,0 +1,13 @@
+package projektiKNK;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+
