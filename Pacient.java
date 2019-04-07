00c55be51565c0a4dd2781e63471bac9915e57b8
Genc i <gencibalaj@gmail.com>
2019-04-07 17:14:54 +0200

    q

---
+++ Pacient.java
@@ -0,0 +1,16 @@
+package projektiKNK;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import java.util.List;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
