ca045fc6e90ca2941239b681929068974fd008f1
Genc i <eltimusa4@gmail.com>
2019-04-10 17:14:54 +0200

    q

---
+++ ShowPacients.java
@@ -0,0 +1,21 @@
+package projektiKNK;
+
+import java.sql.Date;
+import java.util.List;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ShowPacients extends VBox{
+
