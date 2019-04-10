73dfe399b3a3cfe7697218290d22aef278f6a89f
Genc i <ditjonthaqi@hotmail.com>
2019-04-10 17:14:54 +0200

    nju

---
+++ Doctor.java
@@ -0,0 +1,11 @@
+package projektiKNK;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.jdbc.PreparedStatement;
+
+public class Doctor {
+	private int id;
