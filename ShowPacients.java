3a4e49e7a8cfe335f36a4a237802d7075f2f8f0a
Genc i <eltimusa4@gmail.com>
2019-04-10 17:14:54 +0200

    q

--- ShowPacients.java
+++ ShowPacients.java
@@ -19,3 +19,38 @@ import javafx.scene.layout.VBox;
 
 public class ShowPacients extends VBox{
 
+	String cssLayout = 
+            "-fx-font-size: 20px;\n"+
+            "-fx-effect: dropshadow(gaussian,lightgrey,2,2,1,1);" ;
+	private TableColumn<Pacient,String> 	column1 = new TableColumn<>("First Name");	
+	private TableColumn<Pacient,String>	column2 = new TableColumn<>("Last Name");	
+	private TableColumn<Pacient,Date> 	column3 = new TableColumn<>("Birthday");	
+	private TableColumn<Pacient,Boolean> column4 = new TableColumn<>("Gender");	
+	private TableColumn<Pacient,String> 	column5 = new TableColumn<>("RHFactory");	
+	private TableColumn<Pacient,String> 	column6 = new TableColumn<>("Blood Type");	
+	private TableColumn<Pacient,String> 	column7 = new TableColumn<>("Place");	
+	private TableColumn<Pacient,String> 	column8 = new TableColumn<>("Vaccinations");	
+	private TableColumn<Pacient,String> 	column9 = new TableColumn<>("Allergiers");	
+	private TableColumn<Pacient,String> 	column10 = new TableColumn<>("Personal NR");	
+	ScrollPane s=new ScrollPane();
+
+	TableView<Pacient> table = new TableView<>();
+	public ShowPacients()
+	{
+		super(20);
+		setStyle("-fx-background-color:pink;");
+		
+		HBox buttons = new HBox(10);
+		Button Delete=new Button("Delete");
+		Button Update=new Button("Update");
+		buttons.getChildren().addAll(Delete,Update);
+		
+		table.prefHeightProperty().bind(this.heightProperty());
+		table.setStyle(cssLayout);
+		getChildren().addAll(buttons,table);
+		
+		table.setEditable(true);
+		
+		column1.setCellValueFactory(new PropertyValueFactory("fname"));
+		column1.setPrefWidth(100);
+	
