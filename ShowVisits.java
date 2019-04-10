78d2283be734edc36586dd6d3417ec8686d6845f
Genc i <ditjonthaqi@hotmail.com>
2019-04-10 17:14:54 +0200

    eee

--- ShowVisits.java
+++ ShowVisits.java
@@ -15,3 +15,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+public class ShowVisits extends VBox {
+	String cssLayout = 
+            "-fx-font-size: 20px;\n"+
+            "-fx-effect: dropshadow(gaussian,lightgrey,2,2,1,1);" ;
+	Button Delete=new Button("Delete");
+		
