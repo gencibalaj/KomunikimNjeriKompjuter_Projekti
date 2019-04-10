12f9644c99af30c52d314c6174aebea94939a337
Genc i <ditjonthaqi@hotmail.com>
2019-04-10 17:14:54 +0200

    nju

--- Doctor.java
+++ Doctor.java
@@ -9,3 +9,15 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class Doctor {
 	private int id;
+	private String dname;
+	private String ldname;
+	private String email;
+	private String password;
+	
+	public Doctor(int id,String dname, String ldname,String email,String password) {
+		this.id = id;
+		this.dname = dname;
+		this.ldname = ldname;
+		this.email = email;
+		this.password = password;
+	}
