0be8efac528982fa4a946a03270dbadf697e9b29
Genc i <ditjonthaqi@hotmail.com>
2019-04-10 17:14:54 +0200

    eee

---
+++ ShowVisits.java
@@ -0,0 +1,17 @@
+package projektiKNK;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.swing.text.StyledEditorKit.AlignmentAction;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
