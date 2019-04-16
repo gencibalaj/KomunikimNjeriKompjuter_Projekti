745ec4d3a1e244938e3fc34af86dba9a44ba01c9
Genc i <gencibalaj@gmail.com>
2019-04-16 20:12:12 +0200

    a

--- Doctor.java
+++ Doctor.java
@@ -106,3 +106,17 @@ public class Doctor {
 		} catch(SQLException ex) {
 			ex.printStackTrace();
 			return false;	
+		}
+	}
+	
+	public static Doctor login(String email, String password) {
+		String query = "SELECT * FROM doctor WHERE email=? AND password=?";
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(query);
+			
+			
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
