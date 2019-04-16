3643b8eb8c5ee49850d0217d4098526921996830
Genc i <gencibalaj@gmail.com>
2019-04-16 20:12:12 +0200

    a

--- Guidelines.java
+++ Guidelines.java
@@ -6,5 +6,17 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class Guidelines {
 
-
+	private int gid;
+	private int did;
+	private int pid;
+	private String gdescription;
+	
+	public Guidelines(int gid,int did,int pid,String gdescription)
+	{
+		this.gid=gid;
+		this.did=did;
+		this.pid=pid;
+		this.gdescription=gdescription;
+	}
+	
 
