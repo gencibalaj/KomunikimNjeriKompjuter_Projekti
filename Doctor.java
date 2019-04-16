985292724bfc5b9ca17e8fe29ed38e929efb292c
Genc i <gencibalaj@gmail.com>
2019-04-16 20:12:12 +0200

    a

--- Doctor.java
+++ Doctor.java
@@ -85,3 +85,24 @@ public class Doctor {
 			PreparedStatement preparedStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(query);
 			preparedStatement.setInt(1, did);
 			return (preparedStatement.executeUpdate() > 0);
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static boolean updateDoctor(int did,String dname, String ldname, String email, String password) {
+		String query = "UPDATE doctor SET dname=?, ldname=?, email=?, password=? WHERE did=?";
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(query);
+			
+			preparedStatement.setString(1, dname);
+			preparedStatement.setString(2, ldname);
+			preparedStatement.setString(3, email);
+			preparedStatement.setString(4, password);
+			preparedStatement.setInt(5, did);
+			
+			return (preparedStatement.executeUpdate() > 0);
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			return false;	
