ea610d5e96b87496cf322a952cf0f4ce1b52ea29
Genc i <eltimusa4@gmail.com>
2019-04-20 20:12:12 +0200

    new file

---
+++ Toolbar.java
@@ -0,0 +1,18 @@
+package projektiKNK;
+
+import java.util.Locale;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+
+p
