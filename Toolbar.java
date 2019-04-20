5d2f2da09b70f457fba9becbab8a7bf5d263030c
Genc i <eltimusa4@gmail.com>
2019-04-20 20:12:12 +0200

    new file

--- Toolbar.java
+++ Toolbar.java
@@ -15,4 +15,16 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
-p
+public class Toolbar extends ToolBar {
+	
+	Button addButton;
+	Button deleteButton;
+
+	public Toolbar() {	
+		String[] buttons = {"add","remove","update","showPatient","s","addVisits","al","en"};
+		String[] buttonsInfo = {"Add Pacient","Show Visits","Show Visits","Show Visits","s","Add Visit","Set Language to albanian","Set Language to english"};
+		
+		for(int i = 0; i < buttons.length; i++) {
+			if(buttons[i].contentEquals("s")) {
+				Separator separator = new Separator(Orientation.VERTICAL);
+				getItems().add(separator);
