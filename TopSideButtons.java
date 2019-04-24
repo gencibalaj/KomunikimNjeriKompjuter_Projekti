b828cb7a44c3f6e09e28c36ec6a41453b41b5377
Genc i <ditjonthaqi@hotmail.com>
2019-04-24 10:12:14 +0200

    e

--- TopSideButtons.java
+++ TopSideButtons.java
@@ -12,3 +12,31 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 
+public class TopSideButtons extends HBox{
+	private static Button AddPacient=new Button("Add Pacient");
+	private static Button ShowPacients=new Button("Show Pacients");
+	public static Button AddVisit=new Button("Add Visit");
+	private static Button ShowVisit=new Button("Show Visits");
+	public static  Button PrintGuidance=new Button("Print Guidance");
+	
+	public static  TextField searchBar = new TextField();	
+	public static TextField searchBarVisit = new TextField();	
+	
+	String cssLayout = "-fx-border-color: black;\n" +
+            "-fx-border-insets: 5;\n" +
+            "-fx-border-width: 0;\n"+
+            "-fx-font-size: 40px;-fx-background-color: linear-gradient( #f4f5f5, #dfdddd);\n"
+            + "-fx-effect: dropshadow(gaussian,lightgrey,2,2,1,1);" 
+            ;
+	
+	String cssButtom = "-fx-background-color: linear-gradient(to left, #f4f5f5, #dfdddd);"
+			+"-fx-effect: dropshadow(gaussian,lightgrey,2,2,1,1);" ;
+	public TopSideButtons()
+	{		//
+	//	getStylesheets().add(getClass().getResource("file:///C:/Users/ditjo/Desktop/leftButtons.css").toExternalForm());
+			getStylesheets().add("projektiKNK/addPacient.css");
+			ToggleButtons(true);
+			setStyle(cssLayout);
+			setAlignment(Pos.CENTER);
+			setSpacing(25);
+	
