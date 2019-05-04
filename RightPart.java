f7c8a487e89a0a2821e576ee9bdfff7700904432
Genc i <ditjonthaqi@hotmail.com>
2019-05-04 14:12:14 +0200

    qqa

--- RightPart.java
+++ RightPart.java
@@ -10,3 +10,23 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+
+public class RightPart extends VBox{
+	Image image = new Image("file:///C:/Users/ditjo/eclipse-workspace/OrdinancaMjeksore/src/projektiKNK/ordi2.png");
+	ImageView img=new ImageView();
+	
+	String cssLayout ="-fx-effect: dropshadow(gaussian,lightgrey,2,2,1,1);" ;
+	
+		public RightPart()
+		{
+			Toolbar t = new Toolbar();
+			t.setOrientation(Orientation.VERTICAL);
+			getChildren().add(t);
+			setStyle("-fx-backgroud-color:red;");
+			//System.out.println("Eltioni");
+		/*
+		 *  img.setImage(image);
+		 * img.setFitHeight(300);
+		 * 
+		 * img.setFitWidth(300); setAlignment(Pos.CENTER); setSpacing(150);
+		 * setPadding(new Insets(10,10,10,10)); getChildren().addAll(img,new
