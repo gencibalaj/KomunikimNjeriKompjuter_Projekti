81232713740f3d714ac16a9394b6bb05b8471143
Genc i <ditjonthaqi@hotmail.com>
2019-05-04 14:12:14 +0200

    qq

---
+++ RightPart.java
@@ -0,0 +1,12 @@
+package projektiKNK;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.TransferMode;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
