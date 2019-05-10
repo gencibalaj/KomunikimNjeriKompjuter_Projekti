ecb7acec2b4033892fdbcf6bd020c45749e62ff5
Genc i <gencibalaj@gmail.com>
2019-05-10 14:12:14 +0200

    q

---
+++ I18N.java
@@ -0,0 +1,18 @@
+package projektiKNK;
+
+import java.awt.List;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
