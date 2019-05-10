960a7b3ab78173b751efd368686a1f24d547b055
Genc i <gencibalaj@gmail.com>
2019-05-10 14:12:14 +0200

    q

---
+++ MenuBar.java
@@ -0,0 +1,13 @@
+package projektiKNK;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class MenuBar extends HBox {
+		
+	private Text File=new Text(50,50,"File");
+	private Text Edit=new Text("Edit");
+	private Text Help=new Text("Help");
+	private Text Account=new Text("Account");
+	
+	
