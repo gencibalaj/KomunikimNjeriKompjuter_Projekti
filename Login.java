c4765ffb54ffa46f5fbd6e771e863585f3f524ca
Genc i <gencibalaj@gmail.com>
2019-05-28 14:12:14 +0200

    w

---
+++ Login.java
@@ -0,0 +1,25 @@
+package projektiKNK;
+
+
+import java.text.ParseException;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+ import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
