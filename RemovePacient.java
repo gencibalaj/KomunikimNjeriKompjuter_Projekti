f3875aaed45de9c8bae361afc5e185ff196bc058
Genc i <ditjonthaqi@hotmail.com>
2019-05-30 23:12:14 +0200

    w

---
+++ RemovePacient.java
@@ -0,0 +1,28 @@
+package projektiKNK;
+
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+
+public class RemovePacient extends ConnectToDB {
+	
+	
+	
+	
+/*	RemovePacient()
+	{
+
+		getColumns().addAll(0,new Text("diti"));
+		
+		
+		
+		
+		
+		
+		
+		
+	} */
+	
+	
+	
+}
