a07a234102209b16e85a537c8bb99320196aa1e7
Genc i <ditjonthaqi@hotmail.com>
2019-05-30 23:12:14 +0200

    w

---
+++ MedicalGuidance.java
@@ -0,0 +1,21 @@
+package projektiKNK;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class MedicalGuidance  {
+
+	
+	
+
+	
+	
+
+	
+	
+	
+}
